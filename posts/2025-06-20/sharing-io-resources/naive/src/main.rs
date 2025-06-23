ebaa2f3a5f63bdf4af9d3a08bea0dc0410dfef4d
conectado <gabrielalejandro7@gmail.com>
2025-06-22 23:46:00 -0300

    naive section of the article

--- posts/2025-06-20/sharing-io-resources/naive/src/main.rs
+++ posts/2025-06-20/sharing-io-resources/naive/src/main.rs
@@ -1,59 +1,131 @@
+use bytes::Bytes;
+use bytes::BytesMut;
 use std::sync::Arc;
-use std::sync::Mutex;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::tcp::OwnedWriteHalf;
 use tokio::net::{TcpListener, TcpStream};
 #[tokio::main]
 async fn main() {
-    // Shared state: mapping from number to TCP stream let connections: Arc<Mutex<Vec<Arc<TcpStream>>>> = Arc::new(Mutex::new(Vec::new())); let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap(); println!("Server listening on 127.0.0.1:8080");
-    let router = Arc::new(Router::default());
-
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
-    println!("Server listening on 127.0.0.1:8080");
-
-    loop {
-        let (socket, addr) = listener.accept().await.unwrap();
-        println!("New connection from {}", addr);
-
-        let router = router.clone();
-
-        // Spawn a task to handle this connection
-        tokio::spawn(async move {
-            router.handle_connection(socket).await;
-        });
-    }
+    let router = Arc::new(Router::new());
+    router.handle_connections().await.await.unwrap();
 }
 
 #[derive(Default)]
 struct Router {
-    connections: Mutex<Vec<Arc<tokio::sync::Mutex<TcpStream>>>>,
+    connections: tokio::sync::Mutex<Vec<OwnedWriteHalf>>,
 }
 
 impl Router {
+    pub fn new() -> Router {
+        Default::default()
+    }
+
+    pub async fn handle_connections(self: Arc<Self>) -> tokio::task::JoinHandle<()> {
+        let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+
+        tokio::spawn(async move {
+            loop {
+                let (socket, _) = listener.accept().await.unwrap();
+                let router = self.clone();
+
+                tokio::spawn(async move {
+                    router.handle_connection(socket).await;
+                });
+            }
+        })
+    }
+
     async fn handle_connection(&self, socket: TcpStream) {
-        let socket = Arc::new(tokio::sync::Mutex::new(socket));
-        let id = {
-            let mut connections = self.connections.lock().unwrap();
-            connections.push(socket.clone());
-            connections.len()
-        };
+        let (mut read, mut write) = socket.into_split();
+        {
+            let mut connections = self.connections.lock().await;
+            let id = connections.len();
+            write.write_u32(id as u32).await.unwrap();
+            write.flush().await.unwrap();
+            connections.push(write);
+        }
 
-        socket.lock().await.write_u32(id as u32).await.unwrap();
-        socket.lock().await.flush().await.unwrap();
-
-        let mut buffer = Vec::new();
+        let mut buffer = BytesMut::new();
 
         loop {
-            let n = socket.lock().await.read(&mut buffer).await.unwrap();
+            let n = read.read_buf(&mut buffer).await.unwrap();
             assert!(n != 0);
 
-            let id: u32 = u32::from_ne_bytes(buffer[..4].try_into().unwrap());
-
-            let conn = {
-                let connections = self.connections.lock().unwrap();
-                connections[id as usize].clone()
+            let Ok((dest, m)) = parse_message(&mut buffer) else {
+                continue;
             };
 
-            conn.lock().await.write_all(&buffer[4..]).await.unwrap();
+            let mut connections = self.connections.lock().await;
+            connections[dest as usize].write_all(&m).await.unwrap();
         }
+    }
+}
+
+struct ParseError;
+
+fn parse_message(message: &mut BytesMut) -> Result<(u32, Bytes), ParseError> {
+    if message.len() < 5 {
+        return Err(ParseError);
+    }
+
+    let end = message[4..]
+        .iter()
+        .position(|&c| c == b'\0')
+        .ok_or(ParseError)?
+        + 5;
+
+    let dest: u32 = u32::from_be_bytes(message[..4].try_into().unwrap());
+
+    let data = message.split_to(end).split_off(4).freeze();
+
+    Ok((dest, data))
+}
+
+#[cfg(test)]
+mod tests {
+
+    use std::sync::Arc;
+
+    use tokio::{
+        io::{AsyncReadExt, AsyncWriteExt},
+        net::TcpStream,
+    };
+
+    use crate::Router;
+
+    #[tokio::test]
+    async fn it_works() {
+        const INT_MSG1: &str = "hello, number 2\0";
+        const INT_MSG2: &str = "hello back, number 1\0";
+
+        let router = Arc::new(Router::new());
+        router.handle_connections().await;
+
+        let mut sock1 = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+        let mut sock2 = TcpStream::connect("127.0.0.1:8080").await.unwrap();
+        let id1 = sock1.read_u32().await.unwrap();
+        let id2 = sock2.read_u32().await.unwrap();
+
+        let mut msg1 = Vec::new();
+        msg1.extend_from_slice(&id2.to_be_bytes());
+        msg1.extend_from_slice(INT_MSG1.as_bytes());
+
+        sock1.write_all(&msg1).await.unwrap();
+        sock1.flush().await.unwrap();
+        let mut buf = [0; INT_MSG1.len()];
+        sock2.read_exact(&mut buf).await.unwrap();
+
+        assert_eq!(str::from_utf8(&buf).unwrap(), INT_MSG1);
+
+        let mut msg2 = Vec::new();
+        msg2.extend_from_slice(&id1.to_be_bytes());
+        msg2.extend_from_slice(INT_MSG2.as_bytes());
+
+        sock2.write_all(&msg2).await.unwrap();
+        sock2.flush().await.unwrap();
+        let mut buf = [0; INT_MSG2.len()];
+        sock1.read_exact(&mut buf).await.unwrap();
+
+        assert_eq!(str::from_utf8(&buf).unwrap(), INT_MSG2);
     }
 }
